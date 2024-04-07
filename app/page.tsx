05bd407f06418738347b8538a32cb558c413bfb7
jpoechill <poamrong@rith.com>
2024-04-07 01:47:39 -0700

    refine styles

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,27 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="{$myFont} h-full font-['Open Sans']">
-      <div className="fixed bg-white w-full uppercase text-[#428777] z-10 px-5 md:px-8 border-b-[1px] top-0 flex flex-row items-center justify-between h-[60px]">
-        <div>
+    <main className="h-full">
+      <div className="fixed bg-white w-full text-[#428777] z-10 px-5 md:px-8 border-b-[1px] top-0 flex flex-row items-center justify-between h-[60px]">
+        <div className="flex uppercase font-semibold items-center pt-1 text-md text-[14px]">
           <a href="/">
-            {/* <Image src="/logo_white.svg" width={140} height={30} alt="REAN KHMER" /> */}
-
-            <Image src="/left_title.svg" width={150} height={0} alt="REAN KHMER" />
+            <Image src="/logo_green.svg" width={24} height={100} alt="REAN KHMER" className="inline mr-3 pb-1" />
+            niyeay khmer
           </a>
         </div>
-        <div className="hidden md:flex text-[#428777] flex-row text-[1px] items-center gap-10">
-
-          <Image src="/right_title.svg" width={315} height={0} alt="REAN KHMER" />
+        <div className="hidden md:flex text-[#428777] flex-row text-[14px] items-center gap-10">
+          <div>
+            <div className="text-md inline">
+              <a href="">Contact</a>
+            </div>
+            <a href="/login">
+              <button className="border-[1px] border-[#bdcac7] rounded-md p-2 py-1 mx-3">Login</button>
+            </a>
+            <a href="/login">
+              <button className="text-white bg-[#428777] rounded-md p-2 py-1">Sign Up</button>
+            </a>
+          </div>
+          {/* <Image src="/right_title.svg" width={315} height={0} alt="REAN KHMER" /> */}
           {/* <Image src="/title_blurb.svg" width={300} height={500} alt="INTERACTIVE SHORT STORIES IN KHMER" /> */}
         </div>
       </div>
@@ -23,32 +32,56 @@ export default function Home() {
       <div className="flex justify-between h-screen w-full lg:w-[calc(100%-300px)]">
 
         <div className="top-[60px] fixed w-[300px] pl-11 p-8 h-full text-[#2c3e50] text-[14px] bottom-0 border-r-[1px] ">
+
+
+
           <div className="relative">
-            <div className="-left-3 absolute">
-              Intro
+            <div className="-left-3 font-semibold absolute">
+              Basics
             </div>
           </div>
           <br />
-          Consonants <br />
           Vowels <br />
-          Special Characters <br /><br />
+          Consonants <br />
+          Special Characters <br />
+          Numerals<br /><br />
 
           <div className="relative">
-            <div className="-left-3 absolute">
+            <div className="-left-3 font-semibold absolute">
               Beginner
             </div>
           </div>
           <br />
           Words and Vocabulary <br />
           Conversational Phrases <br /><br />
-          Intermediate
+
+          <div className="relative">
+            <div className="-left-3 font-semibold absolute">
+              Intermediate
+            </div>
+          </div>
           <br />
           Words and Vocabulary <br />
           Conversational Phrases <br /><br />
-          Advance
+          <div className="relative">
+            <div className="-left-3 font-semibold absolute">
+              Advance
+            </div>
+          </div>
           <br />
           Words and Vocabulary <br />
           Conversational Phrases <br /><br />
+
+          <div className="relative">
+            <div className="-left-3 font-semibold absolute">
+              Etc.
+            </div>
+          </div>
+          <br />
+          About<br />
+          Membership<br />
+          Resources <br />
+          Contact<br /><br />
         </div>
         <div className="w-full pl-[300px] h-full top-[60px]">
           <div className="mt-[60px] h-full w-full p-8">
@@ -188,16 +221,43 @@ export default function Home() {
                 <div className="font-light  mt-3 text-[14px] text-[#2c3e50]">
                   Before bed, I like to read a book or listen to music to unwind.
                 </div>
+
+
+                <p>
+                  <span data-text="ពេលសម្រាកអាហារថ្ងៃត្រង់" className="word">
+                    ពេលសម្រាកអាហារថ្ងៃត្រង់
+                  </span>
+                </p>
               </div>
 
 
+
               <div className="top-[60px] hidden lg:block w-[300px] text-[#2c3e50] text-[14px] right-0 fixed p-8 z-1 left-flex h-full bottom-0 border-l-[1px] ">
-                On this page <br /><br />
-                Daily Life and Activities <br />
-                Travel and Tourism <br />
-                Socializing and Relationships <br />
-                Shopping and Dining Out <br />
-                Education and Learning <br />
+                <div className="font-semibold mb-4">
+                  On this page
+                </div>
+                <div className="pl-3 flex flex-col gap-3">
+                  <div className="hover:text-[#428777] cursor-pointer">Daily Life and Activities</div>
+                  <div className="hover:text-[#428777] cursor-pointer">Travel and Tourism</div>
+                  <div className="hover:text-[#428777] cursor-pointer">Socializing and Relationships</div>
+                  <div className="hover:text-[#428777] cursor-pointer">Shopping and Dining Out</div>
+                  <div className="hover:text-[#428777] cursor-pointer">Education and Learning</div>
+                </div>
+              </div>
+
+              <div className="text-sm text-right block pt-6 pb-3">
+                Last updated: 01/01/24
+              </div>
+              <div >
+                <hr />
+              </div>
+              <div className="flex justify-between my-3 pt-3">
+                <div>
+                  ← Previous
+                </div>
+                <div>
+                  Next →
+                </div>
               </div>
             </div>
           </div>
