48bc3574bc283c796927936b9313c6ddc5e90b0d
jpoechill <poamrong@rith.com>
2024-04-07 13:15:31 -0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -8,6 +8,7 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
+    '/', '/account'
     /*
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
@@ -15,6 +16,8 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+
+  // matcher: ['/', '/account'],
+    // '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
