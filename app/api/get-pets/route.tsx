e86faaa68642d75bb475b8046a452ff55836297e
jpoechill <poamrong@rith.com>
2024-04-07 01:51:22 -0700

    refine styles

--- app/api/get-pets/route.tsx
+++ app/api/get-pets/route.tsx
@@ -1,11 +1,11 @@
-import { sql } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
-import { unstable_noStore as noStore } from 'next/cache';
+// import { sql } from '@vercel/postgres';
+// import { NextResponse } from 'next/server';
+// import { unstable_noStore as noStore } from 'next/cache';
 
-export async function GET() {
-    noStore();
+// export async function GET() {
+//     noStore();
 
-    const pets = await sql`SELECT * FROM Pets;`;
-    console.log(pets)
-    return NextResponse.json({ pets }, { status: 200 });
-}
+//     const pets = await sql`SELECT * FROM Pets;`;
+//     console.log(pets)
+//     return NextResponse.json({ pets }, { status: 200 });
+// }
